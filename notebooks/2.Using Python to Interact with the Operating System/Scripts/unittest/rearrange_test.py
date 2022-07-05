72ec30e6a6f0af4be2a8ce5b79e57848537ff70e
jesussantana <jesus@esmarketingdigital.com>
2022-07-05 09:16:02 +0200

    Practice Notebook - Unit Tests and Edge Cases

--- notebooks/2.Using Python to Interact with the Operating System/Scripts/unittest/rearrange_test.py
+++ notebooks/2.Using Python to Interact with the Operating System/Scripts/unittest/rearrange_test.py
@@ -6,10 +6,26 @@ import unittest
 class TestRearrange(unittest.TestCase):
     def test_basic(self):
         testcase = "Lovelace, Ada"
-        expected = "Ada lovelace"
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
+        
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+        
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+        
+    def test_one_name(self):
+        testcase = "Vaughn"
+        expected = "Vaughn"
         self.assertEqual(rearrange_name(testcase), expected)
         
 unittest.main()
+
 
 # chmod +x rearrange_test.py
 # ./rearrange_test.py
